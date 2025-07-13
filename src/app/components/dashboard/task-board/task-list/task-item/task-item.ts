2b1e44913747e700e4cedb7ec7c429068abfb878
Akhil1sharma <akhilsharma01501@gmail.com>
2025-07-13 20:20:42 +0530

    update it

--- src/app/components/dashboard/task-board/task-list/task-item/task-item.ts
+++ src/app/components/dashboard/task-board/task-list/task-item/task-item.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Task } from '../../task-interface';
 
 @Component({
   selector: 'app-task-item',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './task-item.html',
   styleUrl: './task-item.css'
 })
 export class TaskItem {
+  @Input() task!: Task;
+  @Output() toggle = new EventEmitter<Task>();
+  @Output() delete = new EventEmitter<Task>();
 
+  onToggle() {
+    this.toggle.emit(this.task);
+  }
+
+  onDelete() {
+    const confirmed = confirm(`Are you sure you want to delete "${this.task.title}"?`);
+    if (confirmed) {
+      this.delete.emit(this.task);
+    }
+  }
 }
